f1f17105d35f9cfb5edba0c9cf48ce12519b82c1
Alphaeu <dealphaeus@gmail.com>
2024-04-24 12:35:44 -0700

    Advanced task

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,26 +1,14 @@
-// 100-await.js
+import { uploadPhoto, createUser } from './utils';
 
-import { uploadPhoto, createUser } from './utils.js';
+export default async function asyncUploadUser() {
+  let res = {};
 
-const asyncUploadUser = async () => {
   try {
-    // Call the uploadPhoto and createUser functions using async/await
-    const photoResponse = await uploadPhoto();
-    const userResponse = await createUser();
-
-    // Return an object with the responses
-    return {
-      photo: photoResponse,
-      user: userResponse,
-    };
-  } catch (error) {
-    // If an error occurs in either function, return an empty object
-    return {
-      photo: null,
-      user: null,
-    };
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
   }
-};
-
-export default asyncUploadUser;
-
+  return res;
+}
